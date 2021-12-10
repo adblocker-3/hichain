e3446886e1907e33a54f25522d3541ea1c17ca39
APANA49 <aungphonenaing57@gmail.com>
2021-12-10 15:27:23 +0800

    Updated addresses

--- zkSync payout.js
+++ zkSync payout.js
@@ -4,6 +4,13 @@
 
 import * as zksync from "zksync";
 
+const contractAddress = "0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3";
+const contract = new Web3Client.eth.Contract(contractABI, contractAddress);
+
+const senderAddress = "0x0835D4a1494905fb3f9fEa60ADcbb937ED2fEbf3";
+const senderPrivateKey = "ede7b538f9bea69191691d06ac1877e6b3f55c1335db60579d8ec293a93adf21";
+const recieverAddress = "0x098F13Eb5D9e57C4Cdccf29e6232dFdF06D44801";
+
 // connect to zkSync network
 const syncProvider = await zksync.getDefaultProvider("localhost");
 const ethersProvider = ethers.getDefaultProvider("localhost");
@@ -42,16 +49,18 @@ await withdraw.awaitVerifyReceipt();
 
 const erc20Id = await ethProxy.resolveTokenId("0xFab46E002BbF0b4509813474841E0716E6730136"); // update accordingly
 
+const nounce_no =  Web3Client.eth.getTransactionCount(senderAddress)
+
 const syncHttpProvider = await zksync.getDefaultProvider("localhost");
 const signedTransferTx = {
   accountId: 13, // id of the sender account in the zkSync
   type: "Transfer",
-  from: "0x..address1",
-  to: "0x..address2",
+  from: senderAddress,
+  to: recieverAddress,
   token: erc20Id,
   amount: claim,
   fee: gas,
-  nonce: 0,
+  nonce: nounce_no,
   signature: {
     pubKey: "dead..", // hex encoded packed public key of signer (32 bytes)
     signature: "beef..", // hex encoded signature of the tx (64 bytes)
