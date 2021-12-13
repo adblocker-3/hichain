8fe0e12956bb593ad04e8a699403999ac3ca6ca0
joshuan98 <dzhei98@gmail.com>
2021-12-13 17:32:14 +0800

    react-router-dom

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
+import Settings from './components/Settings';
+import Policies from './components/Policies';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className='App'>
+      <Route exact path='/' component={Login} />
+      <Route exact path='/Settings' component={Settings} />
+      <Route exact path='/Policies' component={Policies} />
     </div>
+    </Router>
   );
 }
 
---
+++ web-app/src/components/Policies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Policies(){
+    return(
+        <div>
+            <h1>
+                My Policies
+            </h1>
+        </div>
+    );
+}
+
+export default Policies;
