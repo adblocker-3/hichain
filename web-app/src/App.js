5b5c6094edeb8e213dd5353a867a294ee41ac4ec
joshuan98 <dzhei98@gmail.com>
2021-12-13 22:32:48 +0800

    Login
    SignIn changed to Login

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import LandingPage from './components/LandingPage'
-import LoginPage from './components/SignIn'
-import RegisterPage from './components/Register'
-import ForgetPasswordPage from './components/ForgetPassword'
+import Login from './components/Login'
+import Register from './components/Register'
+import ForgetPassword from './components/ForgetPassword'
 import Policies from './components/Policies'
 import Settings from './components/Settings';
 
@@ -16,9 +16,9 @@ function App() {
       <div>
           <Switch>
               <Route exact path="/" component={ LandingPage } />
-              <Route path="/login" component={ LoginPage } />
-              <Route path="/register" component={ RegisterPage } />
-              <Route path="/forget-password" component={ ForgetPasswordPage } />
+              <Route path="/login" component={ Login } />
+              <Route path="/register" component={ Register } />
+              <Route path="/forget-password" component={ ForgetPassword } />
               <Route path="/home" component={ Policies } />
               <Route path="/policies" component={ Policies } />
               <Route path="/settings" component={ Settings } />
