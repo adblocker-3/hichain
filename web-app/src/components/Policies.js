2af5789718a8f9d8f318b07199dfe755824b6f18
joshuan98 <dzhei98@gmail.com>
2021-12-14 14:42:53 +0800

    web tabs

--- web-app/src/components/Policies.js
+++ web-app/src/components/Policies.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
+import 'react-web-tabs/dist/react-web-tabs.css';
 
 function Policies(){
     return(
         <div>
             <Navbar></Navbar>
-            <h1>
-                My Policies
-            </h1>
+            <Tabs
+            defaultTab="one"
+            onChange={(tabId) => { console.log(tabId) }}
+            >
+                <TabList>
+                <Tab tabFor="one">Tab 1</Tab>
+                <Tab tabFor="two">Tab 2</Tab>
+                <Tab tabFor="three">Tab 3</Tab>
+                </TabList>
+                <TabPanel tabId="one">
+                <p>Tab 1 content</p>
+                </TabPanel>
+                <TabPanel tabId="two">
+                <p>Tab 2 content</p>
+                </TabPanel>
+                <TabPanel tabId="three">
+                <p>Tab 3 content</p>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 }
